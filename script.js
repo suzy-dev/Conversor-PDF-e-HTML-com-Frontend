4a1f259c84e55e6cebe63d4b746c0d6e9c20759f
suzana souza <suzanasouza.caetano@gmail.com>
2024-02-12 17:48:11 -0300

    limpando coisas desnecessarias

--- script.js
+++ script.js
@@ -17,16 +17,3 @@ btnGeneratePDF.addEventListener("click", ()=>{
     html2pdf().set(options).from(content).save()
 
 })
-
-// Gerar HTML
-btnGenerateHTML.addEventListener("click", ()=>{
-
-    const content = document.querySelector('#content').innerHTML
-    const blob = new Blob(content,{type: 'text/html'})
-    const link = document.createElement('a')
-    link.download = 'file.html'
-    document.body.appendChild(link)
-
-    link.click()
-    document.body.removeChild(link)
-})
